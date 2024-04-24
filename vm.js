06759952b7c644f2f1fe612d42d9b9c2cb8141c1
kaneza1234 <167434740+kaneza1234@users.noreply.github.com>
2024-04-24 09:49:29 +0200

    Add files via upload
    This is about my javascript work

---
+++ vm.js
@@ -0,0 +1,7 @@
+var number1 = 10;
+let number2 = 20;
+let sum;
+sum = number1 + number2;
+console.log("number1 is", number1, "and number2 is equal to", number2, "the sum is", sum);
+if (number = 10)
+    console.log("this number is equal to 10");
